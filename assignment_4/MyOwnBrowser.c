ee87d4ed5ccd780bae643f00d07449b463995c99
Kartik Pontula <75580066+alutnopk@users.noreply.github.com>
2023-02-11 00:47:46 +0530

    Update MyOwnBrowser.c
    Added IP and port extraction from input, more polite error messages

--- assignment_4/MyOwnBrowser.c
+++ assignment_4/MyOwnBrowser.c
@@ -1,11 +1,12 @@
 /* 
     CS39006 - Networks Laboratory, Spring Semester 2022-2023
     Assignment 4
-    Members: Ashwani Kumar Kamal (20CS10011), Kartik Pontula (20CS10031)
+    Name: Kartik Pontula
+    Roll No: 20CS10031
     Program Synopsis: Client-side socket program for HTTP requests
     Usage:
-        gcc client.c -o cliexec
-        ./cliexec
+        gcc MyOwnBrowser.c -o cli
+        ./cli
 */
 #include <stdio.h> 
 #include <stdlib.h> 
@@ -17,7 +18,6 @@
 #include <arpa/inet.h> 
 #include <netinet/in.h>
 
-#define SERVPORT 8000
 #define BUFSIZE 256
 void showError(char* s)
 {
@@ -27,10 +27,10 @@ void showError(char* s)
 
 int main()
 {
-    int sockfd;
-    char buf[BUFSIZE], *cmd, *url, *filename;
+    int sockfd, port;
+    char buf[BUFSIZE], *cmd, *url, *filename, *urlcpy, *ip;
     // char ws[] = {'\n', '\r', '\a', '\t', ' ', '\0'};
-    char *ws = " \n\r\a\t";
+    char *ws = " \n\r\a\t", *ipsep = "/:";
     struct sockaddr_in servaddr;
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if(sockfd == -1) showError("Socket creation failure.");
@@ -49,31 +49,62 @@ int main()
         memset(buf, 0, BUFSIZE);
         fgets(buf, BUFSIZE, stdin);
         buf[strlen(buf) - 1] = '\0';
-        cmd = strtok(buf, ws); if(!cmd){printf("oi you almost cost me a segfault, watch it\n"); continue;}
+        cmd = strtok(buf, ws); if(!cmd){printf("Please enter a non-empty command\n"); continue;}
         if(strcmp(cmd, "QUIT") == 0)
         {
-            if(strtok(NULL, ws)) printf("writing just QUIT is enough\n");
+            if(strtok(NULL, ws)) printf("Format: QUIT\n");
             printf("Client terminated.\n"); break;
         }
         else if(strcmp(cmd, "GET") == 0)
         {
-            printf("get.\n");
-            url = strtok(NULL, ws); if(!url) {printf("url wher\n"); continue;}
+            url = strtok(NULL, ws); if(!url) {printf("Missing URL\n"); continue;}
+            if(strtok(NULL, ws)) printf("Format: GET <url>\n");
+            // code to parse IP and port
+            // NOTE: Don't use urlcpy outside this else-if block
+            urlcpy = (char*)malloc((strlen(url)+1)*sizeof(char)); strcpy(urlcpy, url);
+
+            if(strstr(urlcpy, "http://")!=urlcpy){printf("Enter a URL that begins with http:// \n"); continue;}
+            urlcpy += strlen("http://");
+            char* portbegin = rindex((const char*)urlcpy, ':');
+            ip = strtok(urlcpy, ipsep); if(!ip){printf("what did you even enter to get this prompt?\n"); continue;}
+            // at this point in the code, urlcpy is <IPADDR>/etc/filename.jpg (with port number)
+            if(portbegin)
+            {
+                sscanf(portbegin+1, "%d", &port);
+                // ERROR: port entered is not short int
+            }
+            else port = 80;
             printf("URL is %s\n", url);
-            if(strtok(NULL, ws)) printf("tmi dude, GET <url> is enough\n");
+            printf("IP is %s\n", ip);
+            printf("port is %d\n", port);
         }
         else if(strcmp(cmd, "PUT") == 0)
         {
-            printf("put.\n");
             url = strtok(NULL, ws);
             filename = strtok(NULL, ws);
-            if(!url) {printf("url wher\n"); continue;}
-            if(!filename) {printf("filename wher\n"); continue;}
+            if(!url) {printf("Missing URL\n"); continue;}
+            if(!filename) {printf("Missing filename\n"); continue;}
+            if(strtok(NULL, ws)) printf("Format: PUT <url> <filename>\n");
+            // yeah same shit again
+            urlcpy = (char*)malloc((strlen(url)+1)*sizeof(char)); strcpy(urlcpy, url);
+
+            if(strstr(urlcpy, "http://")!=urlcpy){printf("Enter a URL beginning with http:// \n"); continue;}
+            urlcpy += strlen("http://");
+            char* portbegin = rindex((const char*)urlcpy, ':');
+            ip = strtok(urlcpy, ipsep); if(!ip){printf("what did you even enter to get this prompt?\n"); continue;}
+            // at this point in the code, urlcpy is <IPADDR>/etc/filename.jpg (with port number)
+            if(portbegin)
+            {
+                sscanf(portbegin+1, "%d", &port);
+                // ERROR: port entered is not short int
+            }
+            else port = 80;
             printf("URL is %s\n", url);
+            printf("IP is %s\n", ip);
+            printf("port is %d\n", port);
             printf("filename is %s\n", filename);
-            if(strtok(NULL, ws)) printf("tmi dude, PUT <url> <filename> is enough\n");
         }
-        else printf("get good at following protocol you doofus\n");
+        else printf("Unrecognized command.\n");
     }
     return 0;
 }
