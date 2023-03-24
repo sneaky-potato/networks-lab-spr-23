cb4d9611dd37730abfbc5fe26433e824f8c11843
Kartik Pontula <kartikpontula@gmail.com>
2023-03-24 15:46:37 +0530

    Initialization

---
+++ assignment_6/pingnetinfo.c
@@ -0,0 +1,24 @@
+/*
+    CS39006 - Networks Laboratory, Spring Semester 2022-2023
+    Assignment No: 6
+    Group No: 16
+    Members: Ashwani Kumar Kamal (20CS10011), Kartik Pontula (20CS10031)
+    Program Synopsis: Estimation of route, latency and bandwidth of an IP address
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <ctype.h>
+
+int main(int argc, char** argv)
+{
+    
+    return 0;
+}
